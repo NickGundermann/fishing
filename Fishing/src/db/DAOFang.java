db149f8135b114359585b96d5286aeb8ecbf25d2
Niels Gundermann <gundermann.niels.ng@googlemail.com>
2013-05-13 13:55:14 +0200

    samples for DAOs an DataFactory

--- Fishing/src/db/DAOFang.java
+++ Fishing/src/db/DAOFang.java
@@ -2,10 +2,66 @@ package db;
 
 import java.util.List;
 
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.criterion.Property;
+
+import com.sfcontroll.business.Entry;
+import com.sfcontroll.db.DTOCostsEntry;
+import com.sfcontroll.db.DataFactory;
+import com.sfcontrolling.gui.MainGuiLoader;
+
 import definition.Fang;
 
 public class DAOFang {
 
+	  private static SessionFactory sessionFactory = null;
+      public static List<DTOCostsEntry> getEntry(){
+      	Session session = null;
+      	sessionFactory = DataFactory.getSessionFactory();
+      	session = sessionFactory.openSession();
+      	
+      	Criteria criteria = session.createCriteria(DTOCostsEntry.class)
+      	                            .createCriteria("status")
+      	                                .createCriteria("statusType")
+      	                                    .add(Property.forName("id").eq(3));
+      	List<DTOCostsEntry> costListe = criteria.list();
+      	
+      	return costListe;
+      }
+      
+      public static void insertEntry(Entry newEntry)
+      {               
+              Session session = null;
+              try 
+               {
+                      try
+                      {
+                              sessionFactory = DataFactory.getSessionFactory();
+                              session = sessionFactory.openSession();
+                              DTOCostsEntry costEntry = new DTOCostsEntry();
+                              System.out.println("Inserting Record");
+                              Transaction tx = session.beginTransaction();
+                              costEntry.setName(newEntry.getName());
+                              costEntry.setCategory(MainGuiLoader.categoryContainer.get(newEntry.getCategory()));
+                              costEntry.setDate(newEntry.getDate());
+                              session.save(costEntry);
+                              tx.commit();
+                              System.out.println("Done");
+                      }
+                      catch (Exception e)
+                      {
+                              System.out.println(e.getMessage());
+                      }
+                 }
+                      finally
+                      {
+                              session.close();
+                      }
+      }
+	
 	public static List<Fang> getAllFang(){
 		return null;
 	}
--- Fishing/src/db/DataFactory.java
+++ Fishing/src/db/DataFactory.java
@@ -1,5 +1,28 @@
 package db;
 
-public class DataFactory {
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
 
+public class DataFactory {
+	private static ServiceRegistry serviceRegistry;
+
+	private static final SessionFactory sessionFactory;
+	static {
+		try {
+			Configuration configuration = new Configuration();
+			configuration.configure();
+			serviceRegistry = new ServiceRegistryBuilder().applySettings(
+					configuration.getProperties()).buildServiceRegistry();
+			sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+		} catch (Throwable ex) {
+			System.err.println("Failed to create sessionFactory object." + ex);
+			throw new ExceptionInInitializerError(ex);
+		}
+	}
+
+	public static SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
 }
